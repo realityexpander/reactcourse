a432b8d0ed5f622e5b7ea6d63f75877cfae244fa
Chris Athanas <chris@crowdskout.com>
2021-01-24 23:15:56 -0600

    Add increment/decrement

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
-
 export default class Counter extends Component {
+  
+  constructor(props){
+    super(props)
+
+    this.state = {
+      count: props.initialCount
+    }
+  }
+
   render() {
     return (
       <div>
-      <button>-</button>
-      <span>{this.props.initialCount}</span>
-      <button>+</button>
+      <button onClick={()=>this.changeCount(-1)}>-</button>
+      <span>{this.state.count}</span>
+      <button onClick={()=>this.changeCount(+1)}>+</button>
     </div>
     )
   }
+
+  changeCount(amount){
+    this.setState({ count: this.state.count + amount})
+  } 
 }
